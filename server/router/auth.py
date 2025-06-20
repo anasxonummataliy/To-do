0c8a5e8282ca20a0b86118e517cf5e070bf3b454
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-06-20 15:08:12 +0500

    fix

--- server/router/auth.py
+++ server/router/auth.py
@@ -6,9 +6,9 @@ from database.session import get_db
 from schemas.auth import RegisRequest, LogisRequest
 from database.models.users import Users
 from security.hash import hash_password
-
+from security.jwt import create_jwt_token, verify_jwt_token
 router = APIRouter(
-    prefix="Auth",
+    prefix="/auth",
     tags=['auth']
 )
 
@@ -23,10 +23,17 @@ async def register(user_data: RegisRequest, db: AsyncSession = Depends(get_db)):
         if db_user is not None:
             raise HTTPException(
                 detail="Bu email orqali allaqachon ro'yhatdan o'tilgan. Iltimos boshqa email orqali ro'yhatdan o'ting!", status_code=400)
-    user_data.password = hash_password(user_data.password)
-    new_user = Users(**user_data.model_dump())
-    db.add(new_user)
-    await db.commit()
+        user_data.password = hash_password(user_data.password)
+        new_user = Users(**user_data.model_dump())
+        db.add(new_user)
+        await db.commit()
+        await db.refresh(new_user)
 
-    token = 
+        token = create_jwt_token(new_user.id)
+        return {
+            'message' : "Ro'yhatdan o'tish muvaffaqiyatli yakunlandi.",
+            "token" : token,
+            "user_id" : new_user.id
+        }
     except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Server xatoligi {str(e)}")
--- server/security/jwt.py
+++ server/security/jwt.py
@@ -1,9 +1,9 @@
 import jwt
 from datetime import  datetime, timedelta
 from utils.config import settings
+from fastapi import HTTPException
 
-
-SECRET_KEY = settings.secret_key  
+SECRET_KEY = settings.jwt_secret  
 ALGORITHM = "HS256"
 
 
@@ -12,3 +12,12 @@ def create_jwt_token(user_id: int) -> str:
     to_encode = {"id": user_id, "exp": expire}
     token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return token
+
+def verify_jwt_token(token : str) -> dict:
+    try :
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token muddati tugagan.")
+    except jwt.InvalidAlgorithmError:
+        raise HTTPException(status_code=401, detail="Token yaroqsiz. ")

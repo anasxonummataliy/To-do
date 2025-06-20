172a2c5bf9ad8df55cf5db9e0b01dfcac9e3b531
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-06-20 14:35:06 +0500

    fix

--- server/utils/config.py
+++ server/utils/config.py
@@ -2,6 +2,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     db_url: str = "postgresql://username:password@localhost:5432/users.db"
-
+    jwt_secret : str = "wiqdiqdiqbidbqiwb1212beib3"
 
 settings = Settings()

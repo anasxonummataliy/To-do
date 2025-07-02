164770596ca6f633edd64ae81266db9e6452815f
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-07-02 20:06:29 +0500

    fix

--- server/utils/config.py
+++ server/utils/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    db_url: str = "postgresql+asyncpg://username:password@localhost:5432/users.db"
+    db_url: str = "postgresql+asyncpg://anasxonummataliy@localhost:5432/todo_db"
     jwt_secret : str = "wiqdiqdiqbidbqiwb1212beib3"
 
 settings = Settings()

2ea67e8f9e702b6ed0dc5ed8e15e8a2586a35159
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-07-06 11:39:11 +0500

    feat: yangilanishlar kiritildi

--- server/utils/config.py
+++ server/utils/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    db_url: str = "postgresql+asyncpg://anasxonummataliy@localhost:5432/todo_db"
+    db_url: str = "sqlite+aiosqlite:///todo.db"
     jwt_secret : str = "wiqdiqdiqbidbqiwb1212beib3"
 
 settings = Settings()
